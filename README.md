8fdb5cdbd00979aeb2b24adff026cd0f924d522c
tiagocockles6400 <tiagocockles@gmail.com>
2025-04-13 13:21:15 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
-# API Lista de Compras Compartilhada
-Apresentação do MVP do curso **Desenvolvimento Full Stack Básico**
+# Minha API
 
-Essa é a API do projeto de MVP para uma lista de compras compartilhada on line.
+Projeto MVP para a Disciplina **Desenvolvimento Full Stack Avançado** 
 
-O objetivo do sistema é cadastrar as listas de compras e os seus produtos relacionados para que ao final do projeto os usuários possam obter relatórios que informam os valores gastos com mercado.
+Este projeto é uma aplicação frontend desenvolvida em HTML, CSS e JavaScript.
+O objetivo é criar uma interface para que os usuários possam compartilhar listas de compras entre si.
+É permitido criar novas listas, adicionar, alterar e excluir produtos.
 
 ---
 ## Como executar 
@@ -12,10 +13,8 @@ O objetivo do sistema é cadastrar as listas de compras e os seus produtos relac
 Será necessário ter todas as libs python listadas no `requirements.txt` instaladas.
 Após clonar o repositório, é necessário ir ao diretório raiz, pelo terminal, para poder executar os comandos descritos abaixo.
 
-> É fortemente indicado o uso de ambientes virtuais do tipo [virtualenv](https://virtualenv.pypa.io/en/latest/installation.html).
-
 ```
-(env)$ pip install -r requirements.txt
+$ pip install -r requirements.txt
 ```
 
 Este comando instala as dependências/bibliotecas, descritas no arquivo `requirements.txt`.
@@ -23,14 +22,34 @@ Este comando instala as dependências/bibliotecas, descritas no arquivo `require
 Para executar a API  basta executar:
 
 ```
-(env)$ flask run --host 0.0.0.0 --port 5000
+$ flask run --host 0.0.0.0 --port 5000
 ```
 
 Em modo de desenvolvimento é recomendado executar utilizando o parâmetro reload, que reiniciará o servidor
 automaticamente após uma mudança no código fonte. 
 
 ```
-(env)$ flask run --host 0.0.0.0 --port 5000 --reload
+$ flask run --host 0.0.0.0 --port 5000 --reload
 ```
 
 Abra o [http://localhost:5000/#/](http://localhost:5000/#/) no navegador para verificar o status da API em execução.
+
+---
+## Como executar através do Docker
+
+Certifique-se de ter o [Docker](https://docs.docker.com/engine/install/) instalado e em execução em sua máquina.
+
+Navegue até o diretório que contém o Dockerfile e o requirements.txt no terminal.
+Execute **como administrador** o seguinte comando para construir a imagem Docker:
+
+```
+$ docker build -t api_slc .
+```
+
+Uma vez criada a imagem, para executar o container basta executar, **como administrador**, seguinte o comando:
+
+```
+$ docker run -p 5000:5000 api_slc
+```
+
+Uma vez executado, para acessar a API, basta abrir o [http://localhost:5000/#/](http://localhost:5000/#/) no navegador. 
--- app.py
+++ app.py
@@ -21,16 +21,62 @@ lista_tag = Tag(name="Lista", description="Adição, visualização e remoção 
 
 @app.get('/', tags=[home_tag])
 def home():
-    """Redireciona para /openap\i, tela que permite a escolha do estilo de documentação.
+    """Redireciona para /openapi, tela que permite a escolha do estilo de documentação.
     """
     return redirect('/openapi')
 
+
+@app.put('/produto', tags=[produto_tag], responses={"200": ProdutoViewSchema, "409": ErrorSchema, "400": ErrorSchema})
+def upd_produto(form: ProdutoSchema):
+    """Atualiza um Produto na base de dados
+
+    Retorna uma representação dos produtos.
+    """
+    
+    produtoNovo = Produto(
+        id_lista=form.id_lista,
+        nome=form.nome,
+        quantidade=form.quantidade,
+        valor=form.valor)
+
+    logger.debug(f"Atualizando produto de nome: '{produtoNovo.nome}'")
+    try:
+        # criando conexão com a base
+        session = Session()
+        # buscando o produto
+        produtoAntigo = session.query(Produto).filter(Produto.id==form.id).first()
+
+        if produtoAntigo:
+            # produtoAntigo.nome = produtoNovo.nome
+            produtoAntigo.quantidade = produtoNovo.quantidade
+            produtoAntigo.valor = produtoNovo.valor
+
+            # efetivando o comando de update na tabela
+            session.commit()
+            logger.debug(f"Alterado o produto de nome: '{produtoAntigo.nome}'")
+            return apresenta_produto(session.query(Produto).filter(Produto.id==form.id).first()), 200
+        else:
+            error_msg = "Não foi possível atualizar o item :/"
+            logger.warning(f"Erro ao atualizar produto '{produtoAntigo.nome}', {error_msg}")
+            return {"mesage": error_msg}, 400
+        
+    except IntegrityError as e:
+        # como a duplicidade do nome é a provável razão do IntegrityError
+        error_msg = "Produto de mesmo nome já salvo na base :/"
+        logger.warning(f"Erro ao atualizar produto '{produtoNovo.nome}', {error_msg}")
+        return {"mesage": error_msg}, 409
+
+    except Exception as e:
+        # caso um erro fora do previsto
+        error_msg = "Não foi possível salvar o item :/"
+        logger.warning(f"Erro ao atualizar produto '{produtoNovo.nome}', {error_msg}")
+        return {"mesage": error_msg}, 400
 
 @app.post('/produto', tags=[produto_tag], responses={"200": ProdutoViewSchema, "409": ErrorSchema, "400": ErrorSchema})
 def add_produto(form: ProdutoSchema):
     """Adiciona um novo Produto à base de dados
 
-    Retorna uma representação dos produtos de determinada lista.
+    Retorna uma representação dos produtos e comentários associados.
     """
     produto = Produto(
         id_lista=form.id_lista,
@@ -43,36 +89,44 @@ def add_produto(form: ProdutoSchema):
         session = Session()
         # adicionando produto
         session.add(produto)
-        
         # efetivando o comando de adição de novo item na tabela
         session.commit()
-
-        registro = session.query(Lista).filter_by(id=form.id_lista).first()
-        if registro:
-            # Atualizar quantidades e valores da lista
-            registro.qtd_itens = registro.qtd_itens + form.quantidade
-            registro.valor_total = registro.valor_total + form.valor
-
-            # Salvar as alterações
-            session.commit()
-        else:
-            logger.warning("Lista não encontrada.")
-
-        logger.debug(f"Adicionado produto: '{produto.nome}'")
+        logger.debug(f"Adicionado produto de nome: '{produto.nome}'")
         return apresenta_produto(produto), 200
 
     except IntegrityError as e:
         # como a duplicidade do nome é a provável razão do IntegrityError
         error_msg = "Produto de mesmo nome já salvo na base :/"
         logger.warning(f"Erro ao adicionar produto '{produto.nome}', {error_msg}")
-        return {"message": error_msg}, 409
+        return {"mesage": error_msg}, 409
 
     except Exception as e:
         # caso um erro fora do previsto
         error_msg = "Não foi possível salvar novo item :/"
         logger.warning(f"Erro ao adicionar produto '{produto.nome}', {error_msg}")
-        return {"message": error_msg}, 400
+        return {"mesage": error_msg}, 400
 
+
+@app.get('/produto', tags=[produto_tag], responses={"200": ProdutoViewSchema, "404": ErrorSchema})
+def get_produto(query: ProdutoBuscaIdSchema):
+    """Faz a busca por um Produto cadastrado.
+
+    Retorna uma representação da listagem de produtos.
+    """
+    logger.debug(f"Coletando produtos")
+    # criando conexão com a base
+    session = Session()
+    # fazendo a busca
+    produto = session.query(Produto).filter(Produto.id == query.id_produto).all()
+
+    if not produto:
+        # se não há produtos cadastrados
+        return {"produto": ""}, 200
+    else:
+        logger.debug(f"%d produtos encontrados" % len(produto))
+        # retorna a representação de produto
+        print(produto)
+        return apresenta_produtos(produto), 200
 
 @app.get('/produtos', tags=[produto_tag], responses={"200": ListagemProdutosSchema, "404": ErrorSchema})
 def get_produtos(query: ProdutoBuscaSchema):
@@ -97,43 +151,30 @@ def get_produtos(query: ProdutoBuscaSchema):
 
 
 @app.delete('/produto', tags=[produto_tag], responses={"200": ProdutoDelSchema, "404": ErrorSchema})
-def del_produto(query: ProdutoBuscaProdutoSchema):
+def del_produto(query: ProdutoBuscaIdSchema):
     """Deleta um Produto a partir do id de produto informado
 
     Retorna uma mensagem de confirmação da remoção.
     """
-    logger.debug(f"Deletando dados sobre produto #{query.id}")
+    logger.debug(f"Deletando dados sobre produto #{query.id_produto}")
     # criando conexão com a base
     session = Session()
     # fazendo a remoção
-    produto = session.query(Produto).filter(Produto.id == query.id).first()
-    count = session.query(Produto).filter(Produto.id == query.id).delete()
+    count = session.query(Produto).filter(Produto.id == query.id_produto).delete()
     session.commit()
-
-    registro = session.query(Lista).filter_by(id=produto.id_lista).first()
-    if registro:
-        # Atualizar quantidades e valores da lista
-        registro.qtd_itens = registro.qtd_itens - produto.quantidade
-        registro.valor_total = registro.valor_total - produto.valor
-
-        # Salvar as alterações
-        session.commit()
-    else:
-        logger.warning("Lista não encontrada.")
 
     if count:
         # retorna a representação da mensagem de confirmação
-        logger.debug(f"Deletado produto #{query.id}")
-        return {"message": "Produto removido", "id": query.id}
+        logger.debug(f"Deletado produto #{query.id_produto}")
+        return {"mesage": "Produto removido", "id": query.id_produto}
     else:
         # se o produto não foi encontrado
         error_msg = "Produto não encontrado na base :/"
-        logger.warning(f"Erro ao deletar produto #'{query.id}', {error_msg}")
-        return {"message": error_msg}, 404
+        logger.warning(f"Erro ao deletar produto #'{query.id_produto}', {error_msg}")
+        return {"mesage": error_msg}, 404
 
 
-@app.post('/lista', tags=[lista_tag],
-          responses={"200": ListaViewSchema, "409": ErrorSchema, "400": ErrorSchema})
+@app.post('/lista', tags=[lista_tag], responses={"200": ListaViewSchema, "409": ErrorSchema, "400": ErrorSchema})
 def add_lista(form: ListaSchema):
     """Adiciona uma nova Lista à base de dados
 
@@ -159,17 +200,16 @@ def add_lista(form: ListaSchema):
         # como a duplicidade do nome é a provável razão do IntegrityError
         error_msg = "Lista de mesma nome e tipo já salva na base :/"
         logger.warning(f"Erro ao adicionar lista '{lista.nome}', {error_msg}")
-        return {"message": error_msg}, 409
+        return {"mesage": error_msg}, 409
 
     except Exception as e:
         # caso um erro fora do previsto
         error_msg = "Não foi possível salvar novo item :/"
         logger.warning(f"Erro ao adicionar lista '{lista.nome}', {error_msg}")
-        return {"message": error_msg}, 400
+        return {"mesage": error_msg}, 400
 
 
-@app.get('/listas', tags=[lista_tag],
-         responses={"200": ListagemListasSchema, "404": ErrorSchema})
+@app.get('/listas', tags=[lista_tag], responses={"200": ListagemListasSchema, "404": ErrorSchema})
 def get_listas():
     """Faz a busca por todos as listas cadastradas
 
@@ -191,8 +231,7 @@ def get_listas():
         return apresenta_listas(listas), 200
 
 
-@app.get('/lista', tags=[lista_tag],
-         responses={"200": ListaViewSchema, "404": ErrorSchema})
+@app.get('/lista', tags=[lista_tag], responses={"200": ListaViewSchema, "404": ErrorSchema})
 def get_lista(query: ListaBuscaSchema):
     """Faz a busca por uma lista a partir do id da lista
 
@@ -209,7 +248,7 @@ def get_lista(query: ListaBuscaSchema):
         # se a lista não foi encontrado
         error_msg = "Lista não encontrado na base :/"
         logger.warning(f"Erro ao buscar lista '{lista_id}', {error_msg}")
-        return {"message": error_msg}, 404
+        return {"mesage": error_msg}, 404
     else:
         logger.debug(f"Lista encontrada: '{lista.nome}'")
         # retorna a representação de lista
@@ -218,7 +257,7 @@ def get_lista(query: ListaBuscaSchema):
 
 @app.delete('/lista', tags=[lista_tag], responses={"200": ListaDelSchema, "404": ErrorSchema})
 def del_lista(query: ListaBuscaSchema):
-    """Deleta uma lista a partir do id da lista informada e todos os seus produtos
+    """Deleta uma lista a partir do id da lista informada
 
     Retorna uma mensagem de confirmação da remoção.
     """
@@ -226,20 +265,14 @@ def del_lista(query: ListaBuscaSchema):
     logger.debug(f"Deletando dados sobre lista #{query.id}")
     # criando conexão com a base
     session = Session()
-    # fazendo a remoção dos produtos
-    count = session.query(Produto).filter(Produto.id_lista == query.id).delete()
-    session.commit()
-
-    # fazendo a remoção da lista
+    # fazendo a remoção
     count = session.query(Lista).filter(Lista.id == query.id).delete()
     session.commit()
-
-    
 
     if count:
         # retorna a representação da mensagem de confirmação
         logger.debug(f"Deletado lista #{query.id}")
-        return {"message": "lista removida", "id": query.id}
+        return {"mesage": "lista removida", "id": query.id}
     else:
         # se a lista_id não foi encontrada
         error_msg = "Lista não encontrada na base :/"
